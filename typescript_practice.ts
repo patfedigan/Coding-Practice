de5e550ecebece50249e7c7ba319787a299872d2
patfedigan <patfedigan@gmail.com>
2025-06-24 20:03:54 -0700

    practice with typscript initial commit

---
+++ typescript_practice.ts
@@ -0,0 +1,16 @@
+// 1. Basic Types
+// TODO: Declare a variable called isStudent of type boolean and set it to true.
+// TODO: Declare a variable called score of type number and set it to 100.
+// TODO: Declare a variable called studentName of type string and set it to your name.
+
+// Your code here:
+var isStudent: boolean = true;
+var score: number = 100;
+var studentName: string = "John Doe";
+
+// 2. Functions
+// TODO: Write a function called multiply that takes two numbers and returns their product.
+// TODO: Call the function with the numbers 4 and 5, and store the result in a variable called product.
+
+// Your code here:
+
